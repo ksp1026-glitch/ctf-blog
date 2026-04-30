a374fb2ca9c60278a4d6cfc158b2de3c753eda83
ksp1026-glitch <ksp1026-glitch@users.noreply.github.com>
2026-04-30 14:07:12 +0800

    Deploy: remove crossorigin, use relative paths

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo 🚀 启动 CTF Blog 本地服务器...
-echo.
-echo 访问地址: http://localhost:5173
-echo 按 Ctrl+C 停止服务器
-echo.
-npm run dev
-pause
--- 启动服务器.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0dist"
-start http://localhost:8888
-python -m http.server 8888
-pause
